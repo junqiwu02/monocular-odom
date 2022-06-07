06a04be164d7b3cbd3c51b8873cf83d665e8a915
junqiwu02 <junqiwu02@gmail.com>
2022-06-07 16:40:05 -0400

    working FAST demo

--- src/odom.py
+++ src/odom.py
@@ -1,3 +1,15 @@
+import numpy as np
 import cv2
 
-print(cv2.__version__)
+PATH = './data/deer_walk/cam0/data'
+
+fast = cv2.FastFeatureDetector_create()
+
+img0 = cv2.imread(f'{PATH}/0000000000000000000.png')
+
+kp = fast.detect(img0, mask=None)
+print(f'{len(kp)} keypoints found')
+img0 = cv2.drawKeypoints(img0, kp, outImage=None, color=(255,0,0))
+
+cv2.imshow('img', img0)
+cv2.waitKey()
